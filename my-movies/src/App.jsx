0bf6b875e6fb3ff16009adfbb13589bf54d0c252
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-07-21 15:57:09 +0530

    first commit

---
+++ my-movies/src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes} from 'react-router-dom';
+import Home from './components/Home';
+
+function App() {
+  return (
+      <div className="w-screen h-screen">
+        <Routes>
+          <Route path='/' element={ <Home />} />
+        </Routes>
+      </div>
+  )
+}
+
+export default App
---
+++ my-movies/src/components/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    document.title = 'MovieApp | Homepage'
+  return (
+    <div>
+      Hello Moto
+    </div>
+  )
+}
+
+export default Home
