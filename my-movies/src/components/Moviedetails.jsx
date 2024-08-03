29ae86285ecfa9a9f86c9de9c901c3688a530db2
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-08-03 16:26:30 +0530

    movie details design

--- my-movies/src/components/Moviedetails.jsx
+++ my-movies/src/components/Moviedetails.jsx
@@ -1,10 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 import { asyncloadmovie, removeMovie } from "../store/actions/movieActions";
 import { useEffect } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import Loading from "../components/Loading";
 
 const Moviedetails = () => {
+  const {pathname} = useLocation();
   const { info } = useSelector((state) => state.movie);
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -58,13 +59,14 @@ const Moviedetails = () => {
       {/* part 2 poster and details */}
       <div className="w-full flex ">
         <img
-          className="h-[40vh] w-[20%] object-cover shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)]"
+          className="h-[50vh] w-[20%] object-cover shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)]"
           src={`https://image.tmdb.org/t/p/original/${
             info.detail.backdrop_path || info.detail.poster_path
           }`}
           alt=""
         />
 
+        {/* Movie title */}
         <div className="content text-white ml-[5%]">
           <h1 className="text-5xl font-black">
             {info.detail.title ||
@@ -72,31 +74,55 @@ const Moviedetails = () => {
               info.detail.original_name ||
               info.detail.original_title}
 
-            <small className="text-zinc-200">
+            {/* <small className="text-zinc-200">
               {" "}
               {info.detail.release_date.split("-")[0]}
-            </small>
+            </small> */}
           </h1>
 
-          <div className="flex text-white font-bold items-center gap-x-3 mt-2">
-          <h1>IMDB</h1>
-            <span className=" w-[6vh] h-[6vh] text-md font-semibold bg-yellow-600 text-white rounded-full flex justify-center items-center">
-              {(info.detail.vote_average * 10).toFixed()} <sup>%</sup>
-            </span>
-            <h1>Release Date:</h1>
-            <h1>{info.detail.release_date}</h1>
-            <h1>Genres: {info.detail.genres.map(g => g.name).join(", ")}</h1>
-            <h1>{info.detail.runtime}min</h1>
-          </div>
+          {/* movie information */}
+          <div className="flex flex-col text-zinc-300 font-bold gap-x-3 mt-2">
+            <div className="flex items-center gap-x-3 text-zinc-200">
+              <h1>{info.detail.release_date.split("-")[0]} .</h1>
+              <h1>{info.detail.genres.map((g) => g.name).join(" | ")}</h1>
+              <h1>
+                {Math.floor(info.detail.runtime / 60)}h{" "}
+                {info.detail.runtime % 60}m
+              </h1>
+            </div>
 
+            <h1 className="font-medium text-base my-2 ">
+              {info.detail.tagline}
+            </h1>
+            <p className="font-normal text-base ">{info.detail.overview}</p>
+            <h1 className="text-zinc-300 my-2">
+              <span className="text-zinc-400 pr-4">Languages</span>{" "}
+              {info.translations.join(" ")}
+            </h1>
+
+            <div className="flex items-center gap-x-3 mt-2">
+              <h1>IMDB</h1>
+              <span className="w-[6vh] h-[6vh] text-md font-semibold bg-yellow-600 text-white rounded-full flex justify-center items-center">
+                {(info.detail.vote_average * 10).toFixed()} <sup>%</sup>
+              </span>
+            </div>
+
+            <Link
+              className="mt-5 w-[23%] rounded-lg text-xl font-bold p-2 bg-[#6556cd] bg-opacity-40 hover:bg-opacity-55"
+              to={`${pathname}/trailer`}
+            >
+              <i className="ri-play-large-fill mr-2"></i>
+              Watch Trailer
+            </Link>
+          </div>
         </div>
       </div>
 
       {/* part 3 available on platform */}
-      <div className="w-[80%] flex flex-col gap-y-5 mt-10">
+      <div className="w-[80%] flex flex-col gap-y-5 mt-28">
         {info.watchproviders && info.watchproviders.flatrate && (
           <div className="flex gap-x-10 items-center text-white">
-            <h1>available on platforms</h1>
+            <i className="text-lg font-semibold text-zinc-300 ">Available On</i>
             {info.watchproviders.flatrate.map((f, i) => (
               <img
                 key={i}
@@ -111,7 +137,9 @@ const Moviedetails = () => {
 
         {info.watchproviders && info.watchproviders.rent && (
           <div className="flex gap-x-10 items-center text-white">
-            <h1>available on rent</h1>
+            <i className="text-lg font-semibold text-zinc-300 ">
+              Available On Rent
+            </i>
             {info.watchproviders.rent.map((w, i) => (
               <img
                 key={i}
@@ -126,7 +154,9 @@ const Moviedetails = () => {
 
         {info.watchproviders && info.watchproviders.buy && (
           <div className="flex gap-x-10 items-center text-white">
-            <h1>available to buy</h1>
+            <i className="text-lg font-semibold text-zinc-300 ">
+              Available To Buy
+            </i>
             {info.watchproviders.buy.map((b, i) => (
               <img
                 key={i}
--- my-movies/src/store/actions/movieActions.jsx
+++ my-movies/src/store/actions/movieActions.jsx
@@ -8,6 +8,7 @@ export const asyncloadmovie = (id) => async (dispatch, getState) => {
         const externalid = await axios.get(`/movie/${id}/external_ids`)
         const recommendations = await axios.get(`/movie/${id}/recommendations`)
         const similar = await axios.get(`/movie/${id}/similar`)
+        const translations = await axios.get(`/movie/${id}/translations`)
         const videos = await axios.get(`/movie/${id}/videos`)
         const watchproviders = await axios.get(`/movie/${id}/watch/providers`)
 
@@ -16,6 +17,7 @@ export const asyncloadmovie = (id) => async (dispatch, getState) => {
             externalid: externalid.data,
             recommendations: recommendations.data.results,
             similar: similar.data.results,
+            translations: translations.data.translations.slice(0, 3).map(t => t.english_name),
             videos: videos.data.results.find(m => m.type === 'Trailer'),
             watchproviders: watchproviders.data.results.IN,
         }
