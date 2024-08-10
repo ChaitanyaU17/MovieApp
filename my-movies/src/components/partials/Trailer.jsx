2364f4ca39d5701da4f42e4a83fa2017ae15e309
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-08-10 11:44:42 +0530

    trailer responsive

--- my-movies/src/components/partials/Trailer.jsx
+++ my-movies/src/components/partials/Trailer.jsx
@@ -5,33 +5,32 @@ import { Link } from "react-router-dom";
 import Notfound from "../Notfound";
 
 const Trailer = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const pathname = location.pathname; 
-    const category = pathname.includes("movie") ? "movie" : "tv";
-    const ytvideo = useSelector((state) => state[category].info.videos);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathname = location.pathname; 
+  const category = pathname.includes("movie") ? "movie" : "tv";
+  const ytvideo = useSelector((state) => state[category].info.videos);
 
-    //console.log(ytvideo);
+  return (
+    <div className="absolute bg-[rgba(0,0,0,0.9)] z-[100] top-0 left-0 w-screen h-screen flex items-center justify-center text-white p-4">
+      <Link
+        onClick={() => navigate(-1)}
+        className="absolute hover:text-[#6556cd] ri-close-fill text-3xl top-4 right-4 md:text-4xl"
+      ></Link>
 
-    return (
-      <div className="absolute bg-[rgba(0,0,0,0.9)] z-[100] top-0 left-0 w-screen h-screen flex items-center justify-center text-white">
-        <Link
-          onClick={() => navigate(-1)}
-          className="absolute hover:text-[#6556cd] ri-close-fill text-3xl top-2 right-[5%] "
-        ></Link>
-
-        {ytvideo ? (
-          <ReactPlayer
-            controls
-            height={500}
-            width={1200}
-            url={`https://www.youtube.com/watch?v=${ytvideo.key}`}
-          />
-        ) : (
-          <Notfound />
-        )}
-      </div>
-    );
+      {ytvideo ? (
+        <ReactPlayer
+          controls
+          url={`https://www.youtube.com/watch?v=${ytvideo.key}`}
+          width="100%"
+          height="100%"
+          style={{ maxWidth: '1000px', maxHeight: '600px' }}
+        />
+      ) : (
+        <Notfound />
+      )}
+    </div>
+  );
 }
 
 export default Trailer;
